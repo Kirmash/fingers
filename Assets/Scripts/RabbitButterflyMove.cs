a69a885c7aa6d9b373f8b57b37cd0e5d19c4fdd0
Kirill <Kirmash@gmail.com>
2016-12-03 21:58:50 +0300

    Rabbits scene final

---
+++ Assets/Scripts/RabbitButterflyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class RabbitButterflyMove : MonoBehaviour {
+    GameObject childObj1;
+	GameObject childObj2;
+	
+	void Start() {
+	childObj1 = transform.GetChild(0).GetChild(5).gameObject;
+	childObj2 = transform.GetChild(0).GetChild(6).gameObject;
+	}
+
+	void Update() {
+		StartCoroutine(Nudge());
+		}
+
+
+	IEnumerator Nudge() {
+	if (childObj1.active || childObj2.active ) {
+		while (true) {
+			GetComponent<Rigidbody2D>().AddForce(Random.insideUnitSphere * 1.0f);
+			yield return new WaitForSeconds(0f);
+		}  
+		}
+	}
+
+}
